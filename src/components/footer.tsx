b247fd9cf8ae2aab3a919fb7333575c9737642a5
davydovMikhail <davydov27977@gmail.com>
2024-11-02 16:17:38 +0300

    links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,18 +10,18 @@ const Footer = () => {
             <div className="footer">
             <img src={logo} className="footer__logo" />
             {/* <div className="footer__text">
-                Copyright 2023. LGP CASINO. All Rights Reserved.
+                Copyright 2024. LGP CASINO. All Rights Reserved.
             </div> */}
             <div className="footer__links">
-                <a target="_blank" href="https://lgp.gitbook.io/lucky-gap-casino/" style={{marginRight: "8px"}}>
+                <a target="_blank" href="https://lgpdocs.gitbook.io/lucky-gap-casino" style={{marginRight: "8px"}}>
                     <img src={wp} alt="wp"/>
                 </a>
-                <a target="_blank" href="https://twitter.com/luckygapcasino" style={{marginRight: "8px"}}>
+                <a target="_blank" href="https://x.com/lgpcasino" style={{marginRight: "8px"}}>
                     <img src={x} alt="x"/>
                 </a>
-                <a target="_blank" href="https://t.me/luckygapcasino">
+                {/* <a target="_blank" href="https://t.me/luckygapcasino">
                     <img src={tg} alt="tg"/>
-                </a>
+                </a> */}
             </div>
             </div>
         </div>
